319a8f549e563bdc4b97bf3582d34f598048e719
Eduardo Hernandez <eduasevi1990@icloud.com>
2021-10-24 22:22:07 -0700

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,13 +6,58 @@ export function fetchUserPurchases() {
     payload: [
       {
         _id: 0,
-        title: "purchase 1",
-        amount: 8.02,
+        total: 19.02,
+        orderNumber: "s5d1f8r16s5",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Eduardo Hernadez",
+          shippingAddress: "1234 west street",
+        },
       },
       {
         _id: 1,
-        title: "purchase 2",
-        amount: 19.02,
+        total: 19.02,
+        orderNumber: "s5d1f8r16s5",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Eduardo Hernadez",
+          shippingAddress: "1234 west street",
+        },
+      },
+      {
+        _id: 2,
+        total: 19.02,
+        orderNumber: "s5d1f8r16s5",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "hrte Hernadez",
+          shippingAddress: "1234 west street",
+        },
+      },
+      {
+        _id: 3,
+        total: 19.02,
+        orderNumber: "s5d1f8r16s5",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "max Hernadez",
+          shippingAddress: "1234 west street",
+        },
+      },
+      {
+        _id: 4,
+        total: 19.02,
+        orderNumber: "s5d1f8r16s5",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "edd Hernadez",
+          shippingAddress: "1234 west street",
+        },
       },
     ],
   };
--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -11,7 +11,14 @@ class Purchases extends Component {
     return (
       <div className={`${className} purchases`}>
         {this.props.purchases.map((purchase) => {
-          return <div>{purchase.title}</div>;
+          return (
+            <div key={purchase._id} className="purchases_--purchase purchase">
+              <img
+                className="purchase_-img"
+                src="http://via.placeholder.com/80x80"
+              />
+            </div>
+          );
         })}
       </div>
     );
